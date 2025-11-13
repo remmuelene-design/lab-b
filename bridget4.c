dea176504023f2ee01c614e8ef1d3136a50d2cd5
Rhea Diaz <rheadiaz@Rheas-MacBook-Pro.local>
2025-11-13 22:21:02 +0800

    first assignment

---
+++ bridget4.c
@@ -0,0 +1,22 @@
+// MAGALLANES, REMMUELENE BRIDGET D. DIT 1-2
+#include <stdio.h>
+
+int main (){
+	int time = 9;
+	int time2 = 13;
+
+	if (time == 9) {
+		printf("Good Morning!\n");
+	} 
+	else {
+		printf("Good Afternoon!");
+	}
+
+	if (time2 == 9) {
+		printf("Good Morning!\n");
+	} 
+	else  {
+		printf ("Good Afternoon!");
+	}
+	return 0;
+}
